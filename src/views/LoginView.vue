ca0ff1003880bcbbb010266953de02f79c826fe5
Putra1105 <87384137+Putra1105@users.noreply.github.com>
2022-10-05 17:00:51 +0700

    Initial commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,314 @@
+<template>
+  <div class="container-xxl bg-danger login">
+    <!-- left side -->
+    <section class="login-left mx-auto text-center">
+      <div class="header">
+        <h1 class="header-title">Sign In</h1>
+        <p class="header-desc">Your Social Campaigns</p>
+      </div>
+      <div class="login-alt d-flex justify-content-center">
+        <a href="">
+          <button type="button" class="btn btn-alt m-4">
+            <div class="login-alt-item">
+              <div class="icon">
+                <svg enable-background="new 0 0 24 24" id="Layer_1" version="1.1" viewBox="0 0 24 24" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                  <g>
+                    <path
+                      d="M12,5c1.6167603,0,3.1012573,0.5535278,4.2863159,1.4740601l3.637146-3.4699707   C17.8087769,1.1399536,15.0406494,0,12,0C7.392395,0,3.3966675,2.5999146,1.3858032,6.4098511l4.0444336,3.1929321   C6.4099731,6.9193726,8.977478,5,12,5z"
+                      fill="#F44336"
+                    />
+                    <path
+                      d="M23.8960571,13.5018311C23.9585571,13.0101929,24,12.508667,24,12   c0-0.8578491-0.093689-1.6931763-0.2647705-2.5H12v5h6.4862061c-0.5247192,1.3637695-1.4589844,2.5177612-2.6481934,3.319458   l4.0594482,3.204834C22.0493774,19.135437,23.5219727,16.4903564,23.8960571,13.5018311z"
+                      fill="#2196F3"
+                    />
+                    <path
+                      d="M5,12c0-0.8434448,0.1568604-1.6483765,0.4302368-2.3972168L1.3858032,6.4098511   C0.5043335,8.0800171,0,9.9801636,0,12c0,1.9972534,0.4950562,3.8763428,1.3582153,5.532959l4.0495605-3.1970215   C5.1484375,13.6044312,5,12.8204346,5,12z"
+                      fill="#FFC107"
+                    />
+                    <path
+                      d="M12,19c-3.0455322,0-5.6295776-1.9484863-6.5922241-4.6640625L1.3582153,17.532959   C3.3592529,21.3734741,7.369812,24,12,24c3.027771,0,5.7887573-1.1248169,7.8974609-2.975708l-4.0594482-3.204834   C14.7412109,18.5588989,13.4284058,19,12,19z"
+                      fill="#00B060"
+                    />
+                    <path
+                      d="M12,23.75c-3.5316772,0-6.7072754-1.4571533-8.9524536-3.7786865C5.2453613,22.4378052,8.4364624,24,12,24   c3.5305786,0,6.6952515-1.5313721,8.8881226-3.9592285C18.6495972,22.324646,15.4981079,23.75,12,23.75z"
+                      opacity="0.1"
+                    />
+                    <polygon opacity="0.1" points="12,14.25 12,14.5 18.4862061,14.5 18.587492,14.25  " />
+                    <path d="M23.9944458,12.1470337C23.9952393,12.0977783,24,12.0493774,24,12   c0-0.0139771-0.0021973-0.0274658-0.0022583-0.0414429C23.9970703,12.0215454,23.9938965,12.0838013,23.9944458,12.1470337z" fill="#E6E6E6" />
+                    <path d="M12,9.5v0.25h11.7855721c-0.0157471-0.0825195-0.0329475-0.1680908-0.0503426-0.25H12z" fill="#FFFFFF" opacity="0.2" />
+                    <linearGradient gradientUnits="userSpaceOnUse" id="SVGID_1_" x1="0" x2="24" y1="12" y2="12">
+                      <stop offset="0" style="stop-color: #ffffff; stop-opacity: 0.2" />
+                      <stop offset="1" style="stop-color: #ffffff; stop-opacity: 0" />
+                    </linearGradient>
+                    <path
+                      d="M23.7352295,9.5H12v5h6.4862061C17.4775391,17.121582,14.9771729,19,12,19   c-3.8659668,0-7-3.1340332-7-7c0-3.8660278,3.1340332-7,7-7c1.4018555,0,2.6939087,0.4306641,3.7885132,1.140686   c0.1675415,0.1088867,0.3403931,0.2111206,0.4978027,0.333374l3.637146-3.4699707L19.8414307,2.940979   C17.7369385,1.1170654,15.00354,0,12,0C5.3725586,0,0,5.3725586,0,12c0,6.6273804,5.3725586,12,12,12   c6.1176758,0,11.1554565-4.5812378,11.8960571-10.4981689C23.9585571,13.0101929,24,12.508667,24,12   C24,11.1421509,23.906311,10.3068237,23.7352295,9.5z"
+                      fill="url(#SVGID_1_)"
+                    />
+                    <path
+                      d="M15.7885132,5.890686C14.6939087,5.1806641,13.4018555,4.75,12,4.75c-3.8659668,0-7,3.1339722-7,7   c0,0.0421753,0.0005674,0.0751343,0.0012999,0.1171875C5.0687437,8.0595093,8.1762085,5,12,5   c1.4018555,0,2.6939087,0.4306641,3.7885132,1.140686c0.1675415,0.1088867,0.3403931,0.2111206,0.4978027,0.333374   l3.637146-3.4699707l-3.637146,3.2199707C16.1289062,6.1018066,15.9560547,5.9995728,15.7885132,5.890686z"
+                      opacity="0.1"
+                    />
+                    <path
+                      d="M12,0.25c2.9750366,0,5.6829224,1.0983887,7.7792969,2.8916016l0.144165-0.1375122   l-0.110014-0.0958166C17.7089558,1.0843592,15.00354,0,12,0C5.3725586,0,0,5.3725586,0,12   c0,0.0421753,0.0058594,0.0828857,0.0062866,0.125C0.0740356,5.5558472,5.4147339,0.25,12,0.25z"
+                      fill="#FFFFFF"
+                      opacity="0.2"
+                    />
+                  </g>
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                  <g />
+                </svg>
+              </div>
+              <p class="icon-text">Sing In With Google</p>
+            </div>
+          </button>
+        </a>
+        <a href="">
+          <button type="button" class="btn btn-alt m-4 d-flex">
+            <div class="login-alt-item">
+              <div class="icon">
+                <svg
+                  enable-background="new 0 0 56.693 56.693"
+                  height="56.693px"
+                  id="Layer_1"
+                  version="1.1"
+                  viewBox="0 0 56.693 56.693"
+                  width="56.693px"
+                  xml:space="preserve"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
+                >
+                  <g>
+                    <path
+                      d="M41.777,30.517c-0.062-6.232,5.082-9.221,5.312-9.372c-2.891-4.227-7.395-4.807-8.998-4.873   c-3.83-0.389-7.477,2.256-9.42,2.256c-1.939,0-4.941-2.199-8.117-2.143c-4.178,0.062-8.029,2.43-10.179,6.17   c-4.339,7.527-1.11,18.682,3.118,24.791c2.067,2.986,4.532,6.346,7.766,6.223c3.117-0.123,4.293-2.016,8.061-2.016   s4.826,2.016,8.123,1.953c3.352-0.061,5.477-3.043,7.527-6.041c2.373-3.469,3.35-6.828,3.408-6.998   C48.305,40.433,41.844,37.958,41.777,30.517z"
+                    />
+                    <path d="M35.582,12.229c1.715-2.082,2.877-4.975,2.561-7.855c-2.475,0.1-5.471,1.645-7.248,3.725   c-1.592,1.846-2.984,4.785-2.611,7.613C31.045,15.926,33.861,14.307,35.582,12.229z" />
+                  </g>
+                </svg>
+              </div>
+              <p class="icon-text">Sing In With Apple</p>
+            </div>
+          </button>
+        </a>
+      </div>
+      <p class="grey">------------------------ Or with email --------------------------</p>
+      <div class="login-form">
+        <form action="" class="justify-content-center text-end">
+          <input type="email" placeholder="Email" class="login-form-1 d-block mx-auto" required />
+          <input type="password" placeholder="Password" class="login-form-1 d-block mx-auto" required />
+          <a href="" class="link">
+            <p class="login-form-forgottext">Forgot Password?</p>
+          </a>
+          <button class="login-form-button">
+            <p class="p-1">Submit</p>
+          </button>
+          <div class="d-flex justify-content-center mt-3">
+            <p class="login-form-signuptext">Not a Member yet?</p>
+            <a href="" class="login-form-signuptext"><p class="link">Sign Up</p></a>
+          </div>
+        </form>
+      </div>
+      <div class="footer d-flex justify-content-center mt-5">
+        <a href="" class="link me-4 ms-4">
+          <p>Terms</p>
+        </a>
+        <a href="" class="link me-4 ms-4">
+          <p>Plans</p>
+        </a>
+        <a href="" class="link me-4 ms-4">
+          <p>Contact Us</p>
+        </a>
+      </div>
+    </section>
+    <!-- end of left side -->
+
+    <!-- right side -->
+    <section class="login-right text-center">
+      <div class="login-right-content">
+        <h1 class="login-right-title">METRONIC</h1>
+        <h3 class="login-right-subtitle">Fast, Efficient and Productive</h3>
+        <div class="login-right-description d-flex justify-content-between">
+          <p>
+            In this kind of post, <code class="orange">the blogger</code> introduces a person they've interviewed and provides some backgruond information about "<code class="orange">the interviewee</code> and their work following this is a
+            transcript of the interview
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+* {
+  font-family: 'Poppins', sans-serif;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+.login {
+  height: 100vh;
+  display: flex;
+  .login-left {
+    background-color: #fff;
+    width: 50%;
+    .header {
+      margin-top: 100px;
+      .header-title {
+        font-size: 30px;
+        font-weight: 600;
+        color: #242538;
+      }
+      .header-desc {
+        font-size: 16px;
+        color: #737373;
+        line-height: 34px;
+      }
+    }
+    .login-alt {
+      display: flex;
+      margin-top: 20px;
+      align-items: center;
+      align-content: center;
+      text-align: center;
+      text-decoration: none;
+      .btn-alt {
+        background-color: #fff;
+        :hover {
+          background-color: #737373;
+          transition: 0.3s ease-in-out;
+        }
+      }
+      .login-alt-item {
+        display: flex;
+        align-items: center;
+        align-content: center;
+        text-align: center;
+        text-decoration: none;
+        border: #d7d9df 1px solid;
+        border-radius: 5px;
+        .icon {
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+          background-color: #fff;
+          display: flex;
+          align-items: center;
+          align-content: center;
+          text-align: center;
+          text-decoration: none;
+          justify-content: center;
+          margin-right: 10px;
+          svg {
+            width: 25px;
+            height: 25px;
+          }
+        }
+        .icon-text {
+          font-size: 16px;
+          color: #242538;
+          margin-right: 10px;
+        }
+      }
+    }
+    .grey {
+      margin-top: 20px;
+      margin-bottom: 20px;
+      font-size: 16px;
+      color: #bebeb6;
+      margin-top: 20px;
+      text-align: center;
+    }
+    .login-form {
+      .login-form-1 {
+        margin: 10px;
+        border-radius: 5px;
+        border: #d7d9df 1px solid;
+        height: 40px;
+        width: 470px;
+        margin-top: 30px;
+      }
+      .login-form-forgottext {
+        font-size: 16px;
+        margin-top: 10px;
+        text-align: end;
+        margin-right: 110px;
+      }
+      .login-form-button {
+        margin: auto;
+        display: block;
+        border: #5a9df5 1px solid;
+        border-radius: 5px;
+        background-color: #5a9df5;
+        width: 470px;
+        :hover {
+          background-color: #1d4ed8;
+          border: #1d4ed8 1px solid;
+          transition: 0.3s ease-in-out;
+        }
+        .signuptext {
+          margin-right: 500px;
+          .login-form-signuptext {
+            font-size: 16px;
+            margin-top: 10px;
+            text-align: center;
+            .link {
+              color: #5a9df5;
+              :hover {
+                color: #1d4ed8;
+                transition: 0.3s ease-in-out;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .login-right {
+    width: 50%;
+    background-color: #4174e9;
+    .login-right-content {
+      color: #fff;
+      justify-content: center;
+      .login-right-title {
+        font-size: 30px;
+        font-weight: 600;
+        margin-top: 100px;
+      }
+      .login-right-subtitle {
+        font-size: 26px;
+        margin-top: 390px;
+      }
+      .login-right-description {
+        text-align: center;
+        font-size: 12px;
+        margin-top: 10px;
+        line-height: 24px;
+        max-width: 400px;
+        margin-left: 140px;
+      }
+    }
+  }
+  .link {
+    text-decoration: none;
+    color: #5594fa;
+  }
+}
+p.orange {
+  color: #ff8c00;
+}
+</style>
